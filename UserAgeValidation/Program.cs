2483df021bae042fbb8930da366813fd6f35ae6c
Ottomann <ott.ottender@gmail.com>
2023-09-23 02:59:52 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+Console.WriteLine("Please, enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (isAgeNumber) //== true
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram.");
+    }
+}
+else 
+{
+    Console.WriteLine("Could not register your age.");
+}
+
+
+
+
+   // int userAge = Int32.Parse (Console.ReadLine());
+
+//if (userAge >= 13)
+{
+    //Console.WriteLine($"Welcome to Instagram!");
+}
+//else 
+{
+    //Console.WriteLine($"You are too young to use Instagram.");
+}
